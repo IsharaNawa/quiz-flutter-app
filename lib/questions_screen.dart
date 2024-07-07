10a3f02ff342bfbdcd18047eaec51f800ad63732
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2024-07-08 00:02:33 +0530

    added the question screen styling and question button

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/models/quiz_question.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,8 +13,43 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final QuizQuestion currentQuestion = questions[0];
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello World");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.question,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            onTap: () {},
+            buttonText: currentQuestion.answers[0],
+          ),
+          AnswerButton(
+            onTap: () {},
+            buttonText: currentQuestion.answers[1],
+          ),
+          AnswerButton(
+            onTap: () {},
+            buttonText: currentQuestion.answers[2],
+          ),
+          AnswerButton(
+            onTap: () {},
+            buttonText: currentQuestion.answers[3],
+          ),
+        ],
+      ),
+    );
   }
 }
